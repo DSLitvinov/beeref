2840667681710c2e8bc397cd32d9ea1f1cb18c19
Dmitry Litvinov <nopomuk@yandex.ru>
2025-11-13 22:26:10 +0300

    Integrate icon support in ImageFloatingMenu for enhanced user interface
    - Added icon integration for buttons in ImageFloatingMenu, replacing text labels with icons for a cleaner look.
    - Updated button tooltips to provide context for each action.
    - Improved code organization by loading icons from the BeeAssets module.

--- beeref/widgets/image_floating_menu.py
+++ beeref/widgets/image_floating_menu.py
@@ -4,8 +4,9 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from PyQt6 import QtWidgets
+from PyQt6 import QtGui, QtWidgets
 
+from beeref.assets import BeeAssets
 from beeref.widgets.floating_menu import FloatingMenu
 
 if TYPE_CHECKING:  # pragma: no cover
@@ -19,38 +20,60 @@ class ImageFloatingMenu(FloatingMenu):
     def __init__(self, parent: QtWidgets.QWidget, view: "BeeGraphicsView"):
         super().__init__(parent, view)
 
+        # Load icons
+        assets = BeeAssets()
+        icons_path = assets.PATH.joinpath('icons')
+        opacity_icon = QtGui.QIcon(str(icons_path.joinpath('opacity.svg')))
+        grayscale_icon = QtGui.QIcon(str(icons_path.joinpath('grayscale.svg')))
+        gamut_icon = QtGui.QIcon(str(icons_path.joinpath('gamut.svg')))
+        flip_h_icon = QtGui.QIcon(str(icons_path.joinpath('flip_h.svg')))
+        flip_v_icon = QtGui.QIcon(str(icons_path.joinpath('flip_v.svg')))
+        crop_icon = QtGui.QIcon(str(icons_path.joinpath('crop.svg')))
+
         self.opacity_btn = self.add_button(
-            "Opacity",
+            "",
+            icon=opacity_icon,
             callback=self.view.on_action_change_opacity,
         )
-        self.add_separator()
+        self.opacity_btn.setToolTip("Opacity")
+       
 
         self.grayscale_btn = self.add_button(
-            "Grayscale",
+            "",
+            icon=grayscale_icon,
             callback=self.view.on_action_grayscale,
             checkable=True,
         )
-        self.add_button(
-            "Color Gamut",
+        self.grayscale_btn.setToolTip("Grayscale")
+        gamut_btn = self.add_button(
+            "",
+            icon=gamut_icon,
             callback=self.view.on_action_show_color_gamut,
         )
+        gamut_btn.setToolTip("Color Gamut")
         self.add_separator()
 
-        self.add_button(
-            "Flip H",
+        crop_btn = self.add_button(
+            "",
+            icon=crop_icon,
+            callback=self.view.on_action_crop,
+        )
+        crop_btn.setToolTip("Crop")
+        self.add_separator()
+
+        flip_h_btn = self.add_button(
+            "",
+            icon=flip_h_icon,
             callback=self.view.on_action_flip_horizontally,
         )
-        self.add_button(
-            "Flip V",
+        flip_h_btn.setToolTip("Flip H")
+        flip_v_btn = self.add_button(
+            "",
+            icon=flip_v_icon,
             callback=self.view.on_action_flip_vertically,
         )
-        self.add_separator()
-
-        self.add_button(
-            "Delete",
-            callback=self.view.on_action_delete_items,
-        )
-
+        flip_v_btn.setToolTip("Flip V")
+       
     def show_for_item(self, item: "BeePixmapItem") -> None:
         self.grayscale_btn.setChecked(getattr(item, "grayscale", False))
         super().show_for_item(item)
